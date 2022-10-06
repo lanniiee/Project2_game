a4acfe81ae7415e75c795fe3a48d7dfa03976b61
lanniiee <lav250397@hotmail.com>
2022-10-06 11:51:44 +0100

    new js file

---
+++ files/countries.js
@@ -0,0 +1,137 @@
+const countries = [
+    {
+        country: "England",
+        option1: "London", //correct
+        option2: "Manchester",
+        option3: "Liverpool",
+        option4: "Scotland",
+    },
+
+    {
+        country: "Brazil",
+        option1: "Rio de Janeiro",
+        option2: "Sao Paulo",
+        option3: "Salvador",
+        option4: "Brasilia", //correct
+    },
+
+    {
+        country: "Jamaica",
+        option1: "Kingston", //correct
+        option2: "Montego Bay",
+        option3: "Port Antonio",
+        option4: "Port Royal",
+    },
+
+    {
+        country: "Australia",
+        option1: "Sydney",
+        option2: "Melbourne",
+        option3: "Canberra", //correct
+        option4: "Perth",
+    },
+
+    {
+        country: "Turkey",
+        option1: "Istanbul",
+        option2: "Ankara", //correct
+        option3: "Izmir",
+        option4: "Antalya",
+    },
+
+    {
+        country: "Nigeria",
+        option1: "Lagos",
+        option2: "Abuja", //correct
+        option3: "Ibadan",
+        option4: "Port Harcourt",
+    },
+
+    {
+        country: "Ghana",
+        option1: "Accra", //correct
+        option2: "Kumasi",
+        option3: "Takoradi",
+        option4: "Cape Coast",
+    },
+
+    {
+        country: "Vietnam",
+        option1: "Ho Chi Minh City",
+        option2: "Da Nang",
+        option3: "Ha Noi", //correct
+        option4: "Hue",
+    },
+
+    {
+        country: "China",
+        option1: "Shanghai",
+        option2: "Hong Kong",
+        option3: "Shenzhen",
+        option4: "Beijing", //correct
+    },
+
+    {
+        country: "Thailand",
+        option1: "Phuket",
+        option2: "Bangkok", //correct
+        option3: "Chiang Mai",
+        option4: "Mueang Chiang Rai",
+    },
+
+    {
+        country: "France",
+        option1: "Paris", //correct
+        option2: "Nice",
+        option3: "Marseille",
+        option4: "Bordeaux",
+    },
+
+    {
+        country: "Italy",
+        option1: "Lake Como",
+        option2: "Milan",
+        option3: "Rome", //correct
+        option4: "Venice",
+    },
+
+    {
+        country: "South Korea",
+        option1: "Seoul", //correct
+        option2: "Busan",
+        option3: "Incheon",
+        option4: "Daegu",
+    },
+
+    {
+        country: "Norway",
+        option1: "Bergen",
+        option2: "Oslo", //correct
+        option3: "Alesund",
+        option4: "Trondheim",
+    },
+
+    {
+        country: "Greenland",
+        option1: "Sisimiut",
+        option2: "Ilulissat",
+        option3: "Nuuk", //correct
+        option4: "Tasiilaq",
+    },
+
+    {
+        country: "Greece",
+        option1: "Rhodes",
+        option2: "Athens", //correct
+        option3: "Chania",
+        option4: "Santorini",
+    },
+
+    {
+        country: "North Korea",
+        option1: "Pyongyang", //correct
+        option2: "Nampo",
+        option3: "Sinuiju",
+        option4: "Chongjin",
+    }
+]
