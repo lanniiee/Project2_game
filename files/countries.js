3868a9a6ede5584bcc7bb0fe39910f1103b33453
lanniiee <lav250397@hotmail.com>
2022-10-06 12:43:04 +0100

    added in correct for correct answers

--- files/countries.js
+++ files/countries.js
@@ -1,7 +1,7 @@
 const countries = [
     {
         country: "England",
-        option1: "London", //correct
+        option1: ["London", "correct"],
         option2: "Manchester",
         option3: "Liverpool",
         option4: "Scotland",
@@ -12,12 +12,12 @@ const countries = [
         option1: "Rio de Janeiro",
         option2: "Sao Paulo",
         option3: "Salvador",
-        option4: "Brasilia", //correct
+        option4: ["Brasilia", "correct"],
     },
 
     {
         country: "Jamaica",
-        option1: "Kingston", //correct
+        option1: ["Kingston", "correct"],
         option2: "Montego Bay",
         option3: "Port Antonio",
         option4: "Port Royal",
@@ -27,14 +27,14 @@ const countries = [
         country: "Australia",
         option1: "Sydney",
         option2: "Melbourne",
-        option3: "Canberra", //correct
+        option3: ["Canberra", "correct"],
         option4: "Perth",
     },
 
     {
         country: "Turkey",
         option1: "Istanbul",
-        option2: "Ankara", //correct
+        option2: ["Ankara", "correct"],
         option3: "Izmir",
         option4: "Antalya",
     },
@@ -42,14 +42,14 @@ const countries = [
     {
         country: "Nigeria",
         option1: "Lagos",
-        option2: "Abuja", //correct
+        option2: ["Abuja", "correct"],
         option3: "Ibadan",
         option4: "Port Harcourt",
     },
 
     {
         country: "Ghana",
-        option1: "Accra", //correct
+        option1: ["Accra", "correct"],
         option2: "Kumasi",
         option3: "Takoradi",
         option4: "Cape Coast",
@@ -59,7 +59,7 @@ const countries = [
         country: "Vietnam",
         option1: "Ho Chi Minh City",
         option2: "Da Nang",
-        option3: "Ha Noi", //correct
+        option3: ["Ha Noi", "correct"],
         option4: "Hue",
     },
 
@@ -68,20 +68,20 @@ const countries = [
         option1: "Shanghai",
         option2: "Hong Kong",
         option3: "Shenzhen",
-        option4: "Beijing", //correct
+        option4: ["Beijing", "correct"],
     },
 
     {
         country: "Thailand",
         option1: "Phuket",
-        option2: "Bangkok", //correct
+        option2: ["Bangkok", "correct"],
         option3: "Chiang Mai",
         option4: "Mueang Chiang Rai",
     },
 
     {
         country: "France",
-        option1: "Paris", //correct
+        option1: ["Paris", "correct"],
         option2: "Nice",
         option3: "Marseille",
         option4: "Bordeaux",
@@ -91,13 +91,13 @@ const countries = [
         country: "Italy",
         option1: "Lake Como",
         option2: "Milan",
-        option3: "Rome", //correct
+        option3: ["Rome", "correct"], 
         option4: "Venice",
     },
 
     {
         country: "South Korea",
-        option1: "Seoul", //correct
+        option1: ["Seoul", "correct"],
         option2: "Busan",
         option3: "Incheon",
         option4: "Daegu",
@@ -106,7 +106,7 @@ const countries = [
     {
         country: "Norway",
         option1: "Bergen",
-        option2: "Oslo", //correct
+        option2: ["Oslo", "correct"], 
         option3: "Alesund",
         option4: "Trondheim",
     },
@@ -115,21 +115,21 @@ const countries = [
         country: "Greenland",
         option1: "Sisimiut",
         option2: "Ilulissat",
-        option3: "Nuuk", //correct
+        option3: ["Nuuk", "correct"],
         option4: "Tasiilaq",
     },
 
     {
         country: "Greece",
         option1: "Rhodes",
-        option2: "Athens", //correct
+        option2: ["Athens", "correct"],
         option3: "Chania",
         option4: "Santorini",
     },
 
     {
         country: "North Korea",
-        option1: "Pyongyang", //correct
+        option1: ["Pyongyang", "correct"],
         option2: "Nampo",
         option3: "Sinuiju",
         option4: "Chongjin",
