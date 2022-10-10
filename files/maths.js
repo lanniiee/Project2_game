fbbe8f3df5ed1778cfaf339193ebc7769f526ad6
lanniiee <lav250397@hotmail.com>
2022-10-10 15:51:04 +0100

    maths questions

---
+++ files/maths.js
@@ -0,0 +1,66 @@
+const mathQuestions = [
+    {
+        question: "What is 7 x 9",
+        A: ["63", "correct"],
+        B: ["70", "incorrect"],
+        C: ["79", "incorrect"],
+        D: ["61", "incorrect"],
+    },
+
+    {
+        question: "Simplify 49 : 56",
+        A: ["7 : 9", "incorrect"],
+        B: ["7 : 8", "correct"],
+        C: ["1 : 7", "incorrect"],
+        D: ["cannot be simplify", "incorrect"],
+    },
+
+    {
+        question: "What is 10% of £12",
+        A: ["£12.00", "incorrect"],
+        B: ["£12.10", "incorrect"],
+        C: ["£1.10", "incorrect"],
+        D: ["£1.20", "correct"],
+    },
+
+    {
+        question: "What is a half, of a half, of a half?",
+        A: ["an eigth", "correct"],
+        B: ["a quarter", "incorrect"],
+        C: ["a half", "incorrect"],
+        D: ["a tenth", "incorrect"],
+    },
+
+    {
+        question: "Round 25.078 to 3 significant figures",
+        A: ["25.0", "incorrect"],
+        B: ["250", "incorrect"],
+        C: ["25.1", "correct"],
+        D: ["25.7", "incorrect"],
+    },
+
+    {
+        question: "What is 5 + 7 x 9?",
+        A: ["65", "incorrect"],
+        B: ["81", "incorrect"],
+        C: ["108", "incorrect"],
+        D: ["68", "correct"],
+    },
+
+    {
+        question: "Share £72 in the ratio of 4:2",
+        A: ["£24 : £48", "incorrect"],
+        B: ["£18 : £54", "incorrect"],
+        C: ["£48 : £24", "correct"],
+        D: ["£54 : £18", "incorrect"],
+    },
+
+    {
+        question: "What is 12.657 - -6.38?",
+        A: ["6.277", "incorrect"],
+        B: ["19.037", "correct"],
+        C: ["18.037", "incorrect"],
+        D: ["18.938", "incorrect"],
+    },
+
+]
